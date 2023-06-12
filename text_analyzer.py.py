fb17b2dd22ee173a0f6f88b55d7132675e10c82b
halker1907 <113712157+halker1907@users.noreply.github.com>
2023-06-12 08:39:10 +0300

    Add files via upload

--- text_analyzer.py.py
+++ text_analyzer.py.py
@@ -1,4 +1,4 @@
-"""
+﻿"""
 скачать текст
 загрузить ТХТ файл
 ТОП 10/20 глаголов/существительных/прилагательных в этом тексте
@@ -6,21 +6,37 @@
 """
 
 class TextAnalyzer:
-    def __init__(self, file="text.txt", mode="r", encoding="UTF-8"):
-        self.file = file
+    def __init__(self, file_name="text.txt", mode="r", encoding="UTF-8"):
+        if file_name is None:
+            raise Exception("Не указан файл для анализа!")
+        self.file_name = file_name
         self.mode = mode
         self.encoding = encoding
-        self.open_file()
-        self.make_text()
+        self.read_file()
+        self.prepare_text()
         self.print_text()
 
-    def open_file(self):
-        self.content = open(self.file, self.mode, encoding=self.encoding)
+    def read_file(self):
+        """ Пытается открыть файл и считать его в строку """
+        try:
+            with open(self.file_name, self.mode, encoding=self.encoding) as file:
+                self.content = file
+                self.text = self.content.read()
+        except FileNotFoundError:
+            raise Exception(f"Файл {self.file_name} не найден!")
+        
+        if not self.text:
+            raise Exception("Файл пустой!")
 
-    def make_text(self):
-        self.text = "".join(self.content)
+    def prepare_text(self):
+        """ Приводит текст к нижнему регистру """
+        self.text = self.text.lower()
 
     def print_text(self):
+        """ Выводит строку текста на экран """
         print(self.text)
 
 TextAnalyzer()
+
+
+
